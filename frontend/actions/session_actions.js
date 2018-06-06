2a42a3515b356bffc60adba1eac801b13b5642ff
Travis <travishn@uci.edu>
2018-06-05 17:33:51 -0700

    Set up reducers to create normalized state

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,7 +2,7 @@ import * as SessionAPIUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
-export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 
 export const receiveCurrentUser = (currentUser) => ({
   type: RECEIVE_CURRENT_USER,
@@ -14,7 +14,7 @@ export const logoutCurrentUser = () => ({
 });
 
 export const receiveErrors = (errors) => ({
-  type: RECEIVE_ERRORS,
+  type: RECEIVE_SESSION_ERRORS,
   errors
 });
 
