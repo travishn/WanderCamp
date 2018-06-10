ab64c6cd1908622cadc3f20442a52be3b6663e2c
Travis <travishn@uci.edu>
2018-06-10 02:28:22 -0700

    Create model level validations for booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: bookings
+#
+#  id         :bigint(8)        not null, primary key
+#  listing_id :integer          not null
+#  guest_id   :integer          not null
+#  num_guests :integer          not null
+#  start_date :date             not null
+#  end_date   :date             not null
+#
+
 class Booking < ApplicationRecord
+  validates :listing_id, :guest_id, :start_date, :end_date, :num_guests, presence: true
+
+  belongs_to :listing
+
+  belongs_to :guest,
+    primary: :id,
+    foreign_key: :guest_id,
+    class_name: :User
 end
--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: bookings
+#
+#  id         :bigint(8)        not null, primary key
+#  listing_id :integer          not null
+#  guest_id   :integer          not null
+#  num_guests :integer          not null
+#  start_date :date             not null
+#  end_date   :date             not null
+#
+
 require 'test_helper'
 
 class BookingTest < ActiveSupport::TestCase
