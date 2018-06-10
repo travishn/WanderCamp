bff4c8de919351be27c2cdf589ef47bd6cdb7ff3
Travis <travishn@uci.edu>
2018-06-09 19:19:17 -0700

    Add header styling to home page

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -3,9 +3,24 @@ import React from 'react';
 class Home extends React.Component {
 
   render() {
-    return <div>
+    return (
+      <div className="home-container">
+        <div className="home-header">
+          <div>
+            <h2>Find yourself outside.</h2>
+          </div>
 
-    </div>;
+          <figure>
+            <img src="http://res.cloudinary.com/emanon/image/upload/c_scale,h_540,w_712/v1528489573/photo-1484960055659-a39d25adcb3c.jpg" />
+          </figure>
+        </div>
+
+
+        <div className="home-categories">
+        </div>
+      </div>
+    );
+
   }
 }
 
--- frontend/components/listings/listing_index_item.jsx
+++ frontend/components/listings/listing_index_item.jsx
@@ -4,22 +4,24 @@ import { Link } from 'react-router-dom';
 const ListingsIndexItem = ({ listing, photos }) => {
   return (
     <ul className="listing-index-container">
-      <li className="listing-index-picture">
-        <figure className="listing-index-figure">
-          <img src={photos[listing.id][0]} alt={listing.name} />
-        </figure>
-      </li>
+      <Link to={`/listings/${listing.id}`}>
+        <li className="listing-index-picture">
+          <figure className="listing-index-figure">
+            <img src={photos[listing.id][0]} alt={listing.name} />
+          </figure>
+        </li>
 
-      <li className="listing-index-info">
-        <div className="listing-index-title">
-          {listing.title}
-        </div>
+        <li className="listing-index-info">
+          <div className="listing-index-title">
+            {listing.title}
+          </div>
 
-        <div className="listing-index-description">
-          {listing.description.slice(0, 230)}
-          <span className="trailing-dots">...</span>
-        </div>
-      </li>
+          <div className="listing-index-description">
+            {listing.description.slice(0, 230)}
+            <span className="trailing-dots">...</span>
+          </div>
+        </li>
+      </Link>
     </ul>
   );
 };
