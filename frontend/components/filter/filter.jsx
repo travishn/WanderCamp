ebd2601b19bdc9fd2e4b2456b46c5d0dc2301318
Travis <travishn@uci.edu>
2018-06-13 13:42:17 -0700

    Start working on filter component

--- frontend/components/filter/filter.jsx
+++ frontend/components/filter/filter.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
 
 class Filter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = this.props;
+    // allows for re-rendering upon local state change
+  }
+
 
   render() {
-
+    const { filters } = this.props;
+    
     return (
-      <div>Filter bar goes here</div>
+      <div className="filters-container">
+        <h2>Search Filters</h2>
+        
+
+        <h3>Camping Type</h3>
+        <ul>
+          <li className='filters-list-item'>
+            <label>Camping</label>
+            <input 
+              className='checkbox'
+              type='radio'
+              onClick={this.handleListingFilter('public')}
+              checked={filters['camping'] === true}
+            />
+          </li>
+        </ul>
+
+        <h3>Pricing</h3>
+        <ul>
+        </ul>
+
+        <h3>Group Size</h3>
+        <ul>
+        </ul>
+
+        <h3>Activities</h3>
+        <ul>
+        </ul>
+
+        <h3>Terrain</h3>
+        <ul>
+        </ul>
+
+      </div>
     );
   }
 }
